222f9c48364e6c4c033260a2c89203d2d406d0ed
1076841736 <1076841736@qq.com>
2017-05-22 19:08:50 +0800

    first change

---
+++ app/src/main/java/com/baway/www/gitspace/MainAcitivity_Two.java
@@ -0,0 +1,20 @@
+package com.baway.www.gitspace;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * User : 陈振艳
+ * Date : 2017/5/22
+ * 用途 ：
+ * 思路 ：
+ */
+
+public class MainAcitivity_Two extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
